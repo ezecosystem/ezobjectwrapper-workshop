81ebc23aa6f23aed0b1d69f5a0d834dff28fe206
Steven Bressey <sbressey@kaliop.com>
2015-08-12 16:16:33 +0200

    prepared base controllers & templates

---
+++ src/Kaliop/EzobjectWorkshopBundle/Controller/FolderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kaliop\EzobjectWorkshopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FolderController extends Controller
+{
+    public function indexAction($locationId, $viewType, $layout = false, array $params = array())
+    {
+        /**
+         * @var Repository $repository
+         */
+        $repository = $this->get('ezpublish.api.repository');
+
+        $response =  $this->get( 'ezpublish.controller.content.view' )->viewLocation(
+            $locationId,
+            $viewType,
+            $layout,
+            $params
+        );
+
+        $response->setPublic();
+
+        return $response;
+    }
+}
--- src/Kaliop/EzobjectWorkshopBundle/Controller/HomeController.php
+++ src/Kaliop/EzobjectWorkshopBundle/Controller/HomeController.php
@@ -2,12 +2,32 @@
 
 namespace Kaliop\EzobjectWorkshopBundle\Controller;
 
+use eZ\Publish\API\Repository\Repository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
     public function indexAction($locationId, $viewType, $layout = false, array $params = array())
     {
+        /**
+         * @var Repository $repository
+         */
+        $repository = $this->get('ezpublish.api.repository');
+
+
+        $location = $repository->getLocationService()->loadLocation($locationId);
+        $childrenLocationList = $repository->getLocationService()->loadLocationChildren($location);
+
+        $children = array();
+        foreach($childrenLocationList->locations as $location)
+        {
+            $content = $repository->getContentService()->loadContent($location->contentId);
+            $contentType = $repository->getContentTypeService()->loadContentType($content->contentInfo->contentTypeId);
+            $children[]=array('identifier' => $contentType->identifier, 'content' => $content, 'location' => $location);
+        }
+
+        $params['children']=$children;
+
         $response =  $this->get( 'ezpublish.controller.content.view' )->viewLocation(
             $locationId,
             $viewType,
@@ -16,7 +36,6 @@ class HomeController extends Controller
         );
 
         $response->setPublic();
-        $response->setMaxAge(46200);//12h
 
         return $response;
     }
